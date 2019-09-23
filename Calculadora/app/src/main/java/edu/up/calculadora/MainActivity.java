c31cb318134ec49ef02832ccbc36a44a6c9c20ff
PatrickSLuz <patricksantos20101@gmail.com>
2019-09-23 10:22:47 -0300

    Implementado Calculos com Potencia, Raiz Quadrada e Porcentagem

--- Calculadora/app/src/main/java/edu/up/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/edu/up/calculadora/MainActivity.java
@@ -37,9 +37,9 @@ public class MainActivity extends AppCompatActivity {
         }else if(v.getId() == R.id.btnUm){
             txtResultado.setText(txtResultado.getText() + "1");
         }else if(v.getId() == R.id.btnDois){
-            txtResultado.setText(txtResultado.getText() + "2");
+           txtResultado.setText(txtResultado.getText() + "2");
         }else if(v.getId() == R.id.btnTres){
-            txtResultado.setText(txtResultado.getText() + "3");
+           txtResultado.setText(txtResultado.getText() + "3");
         }else if(v.getId() == R.id.btnQuatro){
             txtResultado.setText(txtResultado.getText() + "4");
         }else if(v.getId() == R.id.btnCinco){
@@ -59,60 +59,133 @@ public class MainActivity extends AppCompatActivity {
 
         if(v.getId() == R.id.btnMais){
             op = "+";
-            valorUm = Double.parseDouble(txtResultado.getText().toString());
+            tamanho_vlr_um = txtResultado.length();
+            if (tamanho_vlr_um > 0){
+                valorUm = Double.parseDouble(txtResultado.getText().toString());
+            }else{
+                txtResultado.setText(valorUm.toString());
+            }
             txtResultado.setText(txtResultado.getText() + " + ");
             btnPonto.setEnabled(true);
             // variavel para receber o tamanho do texto do EditText.
             tamanho_vlr_um = txtResultado.length();
+
         }else if(v.getId() == R.id.btnMenos){
             op = "-";
-            valorUm = Double.parseDouble(txtResultado.getText().toString());
+            tamanho_vlr_um = txtResultado.length();
+            if (tamanho_vlr_um > 0){
+                valorUm = Double.parseDouble(txtResultado.getText().toString());
+            }else{
+                txtResultado.setText(valorUm.toString());
+            }
             txtResultado.setText(txtResultado.getText() + " - ");
             btnPonto.setEnabled(true);
             // variavel para receber o tamanho do texto do EditText.
             tamanho_vlr_um = txtResultado.length();
+
         }else if(v.getId() == R.id.btnDiv){
             op = "/";
-            valorUm = Double.parseDouble(txtResultado.getText().toString());
+            tamanho_vlr_um = txtResultado.length();
+            if (tamanho_vlr_um > 0){
+                valorUm = Double.parseDouble(txtResultado.getText().toString());
+            }else{
+                txtResultado.setText(valorUm.toString());
+            }
             txtResultado.setText(txtResultado.getText() + " / ");
             btnPonto.setEnabled(true);
             // variavel para receber o tamanho do texto do EditText.
             tamanho_vlr_um = txtResultado.length();
+
         }else if(v.getId() == R.id.btnMulti){
             op = "x";
-            valorUm = Double.parseDouble(txtResultado.getText().toString());
+            tamanho_vlr_um = txtResultado.length();
+            if (tamanho_vlr_um > 0){
+                valorUm = Double.parseDouble(txtResultado.getText().toString());
+            }else{
+                txtResultado.setText(valorUm.toString());
+            }
             txtResultado.setText(txtResultado.getText() + " x ");
             btnPonto.setEnabled(true);
             // variavel para receber o tamanho do texto do EditText.
             tamanho_vlr_um = txtResultado.length();
+        }else if(v.getId() == R.id.btnPotencia){
+            op = "pot";
+            tamanho_vlr_um = txtResultado.length();
+            if (tamanho_vlr_um > 0){
+                valorUm = Double.parseDouble(txtResultado.getText().toString());
+            }else{
+                txtResultado.setText(valorUm.toString());
+            }
+            txtResultado.setText(txtResultado.getText() + " pot ");
+            btnPonto.setEnabled(true);
+            // variavel para receber o tamanho do texto do EditText.
+            tamanho_vlr_um = txtResultado.length();
+
+        }else if(v.getId() == R.id.btnProcentagem){
+            op = "%";
+            tamanho_vlr_um = txtResultado.length();
+            if (tamanho_vlr_um > 0){
+                valorUm = Double.parseDouble(txtResultado.getText().toString());
+            }else{
+                txtResultado.setText(valorUm.toString());
+            }
+            txtResultado.setText(txtResultado.getText() + " % ");
+            btnPonto.setEnabled(true);
+            // variavel para receber o tamanho do texto do EditText.
+            tamanho_vlr_um = txtResultado.length();
+
+        }else if (v.getId() == R.id.btnRaiz){
+            op = "raiz";
+            tamanho_vlr_um = txtResultado.length();
+            if (tamanho_vlr_um > 0){
+                valorUm = Double.parseDouble(txtResultado.getText().toString());
+            }else{
+                txtResultado.setText(valorUm.toString());
+            }
+            txtResultado.setText(txtResultado.getText() + " √ ");
+            btnPonto.setEnabled(true);
+            // variavel para receber o tamanho do texto do EditText.
+            tamanho_vlr_um = txtResultado.length();
+
         }
 
         if (v.getId() == R.id.btnLimpar){
             txtResultado.setText("");
+            valorUm = 0.0;
+            valorDois = 0.0;
             tamanho_vlr_um = 0;
         }
 
         if(v.getId() == R.id.btnIgual){
             Double result = 0.0;
 
-            // nesta linha a variavel esta recebendo apenas a String que esta apos o que foi digitado
-            String vlrDois = txtResultado.getText().toString().substring(tamanho_vlr_um-1);
+            if (op.equals("raiz")){
+                result = Math.sqrt(valorUm);
+            }else{
+                // nesta linha a variavel esta recebendo apenas a String que esta apos o que foi digitado
+                String vlrDois = txtResultado.getText().toString().substring(tamanho_vlr_um-1);
+                valorDois = Double.parseDouble(vlrDois);
 
-            valorDois = Double.parseDouble(vlrDois);
+                if(op.equals("+")){
+                    result = valorUm + valorDois;
+                }else if (op.equals("-")){
+                    result = valorUm - valorDois;
+                }else if (op.equals("/")){
+                    result = valorUm / valorDois;
+                }else if (op.equals("x")){
+                    result = valorUm * valorDois;
+                }
 
-            if(op.equals("+")){
-                result = valorUm + valorDois;
-            }else if (op.equals("-")){
-                result = valorUm - valorDois;
-            }else if (op.equals("/")){
-                result = valorUm / valorDois;
-            }else if (op.equals("x")){
-                result = valorUm * valorDois;
+                if(op.equals("pot")){
+                    result = Math.pow(valorUm, valorDois);
+                }else if (op.equals("%")){
+                    result = (valorUm/100) * valorDois;
+                }
             }
 
-            txtResultado.setText(txtResultado.getText() + " = " + String.valueOf(result));
+            txtResultado.setText(String.valueOf(result));
+            valorUm = result; // valor um recebe o resultado caso seja necessário fazer outra operacao com este resultado
             dps_resultado = true;
         }
-        Toast.makeText(this, "Oie", Toast.LENGTH_LONG).show();
     }
 }
