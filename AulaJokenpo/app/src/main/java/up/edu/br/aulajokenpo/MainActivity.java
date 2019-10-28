4920e49fd7ebc5466fe5bbcc478760229864a849
PatrickSLuz <patricksantos20101@gmail.com>
2019-10-28 10:32:08 -0300

    Implementado Jogadas com o Firebase

--- AulaJokenpo/app/src/main/java/up/edu/br/aulajokenpo/MainActivity.java
+++ AulaJokenpo/app/src/main/java/up/edu/br/aulajokenpo/MainActivity.java
@@ -5,15 +5,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    int qntJogadas = 0;
     int pontosJogador = 0;
     int pontosComputador = 0;
 
@@ -23,18 +26,56 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("jokenpo");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot d : dataSnapshot.getChildren()){
+                    Integer jogadorremoto = d.child("jogador").getValue(Integer.class);
+                    Integer escolharemoto = d.child("escolha").getValue(Integer.class);
+
+                    ImageButton btnJogador = findViewById(R.id.btnJogador);
+                    ImageButton btnCopmutador = findViewById(R.id.btnComputador);
+
+                    if (jogadorremoto.equals(jogador)){
+                        btnJogador.setImageResource(escolharemoto);
+                    }else{
+                        btnCopmutador.setImageResource(escolharemoto);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+            }
+        });
+    }
+
+    public void zerar(ImageButton btnPC, ImageButton btnJogador, TextView txtVencedor, TextView txtResultJogador, TextView txtResultPC){
+        btnJogador.setImageResource(R.drawable.vazio);
+        btnPC.setImageResource(R.drawable.vazio);
+        txtVencedor.setText("");
+        txtResultJogador.setText("Você\n"+0);
+        txtResultPC.setText("Adversário\n"+0);
+        pontosJogador = 0;
+        pontosComputador = 0;
     }
 
     public void jogar (View v){
 
-        qntJogadas++;
-
-        int jogadaPC = new Random().nextInt(3) + 1; // Classe para randomizar do Java.
-
         ImageButton btnJogador = findViewById(R.id.btnJogador);
         ImageButton btnCopmutador = findViewById(R.id.btnComputador);
 
+        int jogadaPC = new Random().nextInt(3) + 1; // Classe para randomizar do Java.
+
         TextView txtVencedor = findViewById(R.id.txtVencedor);
+        TextView txtResultPC = findViewById(R.id.txtResultPC);
+        TextView txtResultJogador = findViewById(R.id.txtResultJogador);
 
         Jogada jogada = new Jogada();
         jogada.jogador = jogador;
@@ -56,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("jokenpo");
         myRef.push().setValue(jogada);
-
+/*
         if(jogadaPC == 1){
             btnCopmutador.setImageResource(R.drawable.tesoura);
         }
@@ -66,7 +107,7 @@ public class MainActivity extends AppCompatActivity {
         if(jogadaPC == 3){
             btnCopmutador.setImageResource(R.drawable.pedra);
         }
-
+*/
         if (jogadaPC == 1){
             if(v.getId() == R.id.btnPedra){
                 // jogador ganha
@@ -108,23 +149,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        if (qntJogadas == 15){
-            if(pontosComputador > pontosJogador){
-                // PC vence
-               // Toast.makeText(this, "COMPUTADOR VENCEU", Toast.LENGTH_SHORT).show();
-            }else if (pontosJogador > pontosComputador){
-                // Jogador vence
-              //  Toast.makeText(this, "JOGADOR VENCEU", Toast.LENGTH_SHORT).show();
-            }else{
-                //empate
-               // Toast.makeText(this, "EMPATE", Toast.LENGTH_SHORT).show();
-            }
-            btnJogador.setImageResource(R.drawable.vazio);
-            btnCopmutador.setImageResource(R.drawable.vazio);
-            txtVencedor.setText("");
-            pontosJogador = 0;
-            pontosComputador = 0;
-            qntJogadas = 0;
+        txtResultJogador.setText("Você\n"+pontosJogador);
+        txtResultPC.setText("Adversário\n"+pontosComputador);
+
+        if (pontosJogador == 10) {
+            Toast.makeText(this, "JOGADOR VENCEU", Toast.LENGTH_SHORT).show();
+            zerar(btnCopmutador, btnJogador, txtVencedor, txtResultJogador, txtResultPC);
+        }else if(pontosComputador == 10) {
+            Toast.makeText(this, "COMPUTADOR VENCEU", Toast.LENGTH_SHORT).show();
+            zerar(btnCopmutador, btnJogador, txtVencedor, txtResultJogador, txtResultPC);
+        }else {
+            Toast.makeText(this, "EMPATE", Toast.LENGTH_SHORT).show();
+            zerar(btnCopmutador, btnJogador, txtVencedor, txtResultJogador, txtResultPC);
         }
     }
 }
