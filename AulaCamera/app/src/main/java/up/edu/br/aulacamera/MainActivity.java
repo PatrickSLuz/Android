e75914380ce9035b956287a197774e233b3cf444
PatrickSLuz <patricksantos20101@gmail.com>
2019-11-11 10:47:41 -0300

    Implementado envio de Imagem para o Storage do Firebase

--- AulaCamera/app/src/main/java/up/edu/br/aulacamera/MainActivity.java
+++ AulaCamera/app/src/main/java/up/edu/br/aulacamera/MainActivity.java
@@ -2,11 +2,21 @@ package up.edu.br.aulacamera;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +41,29 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == CODIGO_CAMERA && resultCode == RESULT_OK){
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imagem.setImageBitmap(photo);
+
+            // Transformar uma Imagem em um Array de Bytes
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] arquivo = baos.toByteArray();
+
+            // Gravar a Imagem no Storage do Firebase
+            StorageReference storage = FirebaseStorage.getInstance().getReference(); // Referencia do Storage
+            UploadTask uploadTask = storage.child("imagem.jpg").putBytes(arquivo);
+            //UploadTask uploadTask = storage.child("/Contato.id/imagem.jpg").putBytes(arquivo); // Definir caminho
+
+            // Verificar se gravou no Firebase
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this, "Erro ao gravar a Imagem!", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(MainActivity.this, "Sucesso ao gravar a Imagem!", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
