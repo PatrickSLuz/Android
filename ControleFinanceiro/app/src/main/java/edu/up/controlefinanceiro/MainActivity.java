4f1c809c13cadb51e4631599856d4843b33c2bea
PatrickSLuz <patricksantos20101@gmail.com>
2019-11-23 22:04:14 -0300

    Implementado Menu e Visualizacao por outros Meses

--- ControleFinanceiro/app/src/main/java/edu/up/controlefinanceiro/MainActivity.java
+++ ControleFinanceiro/app/src/main/java/edu/up/controlefinanceiro/MainActivity.java
@@ -1,10 +1,16 @@
 package edu.up.controlefinanceiro;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -26,26 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
     private List<EntradaSaida> lancamentos = new ArrayList<>();
 
+    String mesEanoAtual = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Mes e Ano escolhido para exibir
+        TextView txtMesAnoAtual = findViewById(R.id.txtMesAnoAtual);
+
+        if (getIntent().getExtras() != null){
+            mesEanoAtual = (String) getIntent().getExtras().getSerializable("mesEano");
+            String mes = obterNomeMes(Integer.parseInt(mesEanoAtual.substring(0,2)) -1);
+            String ano = mesEanoAtual.substring(3);
+            txtMesAnoAtual.setText(mes + " de " + ano);
+        }else{
+            Date dataAtual = new Date();
+            mesEanoAtual = new SimpleDateFormat("MM-yyyy").format(dataAtual);
+            String ano = new SimpleDateFormat("yyyy").format(dataAtual);
+            txtMesAnoAtual.setText(obterNomeMes(dataAtual.getMonth()) + " de " + ano);
+        }
+
         getSupportActionBar().setTitle("Controle Financeiro"); // Titulo para ser exibido na sua Action Bar
 
         final TextView txtTotalEntradaMes = findViewById(R.id.txtTotalEntradaMes);
         final TextView txtTotalSaidaMes = findViewById(R.id.txtTotalSaidaMes);
-
-        // Mes e Ano Atual
-        TextView txtMesAnoAtual = findViewById(R.id.txtMesAnoAtual);
-        Date dataAtual = new Date();
-        final String mesEanoAtual = new SimpleDateFormat("MM-yyyy").format(dataAtual);
-        String ano = new SimpleDateFormat("yyyy").format(dataAtual);
-        txtMesAnoAtual.setText(obterNomeMes(dataAtual.getMonth()) + " de " + ano);
-
-        // Menu 3 pontinhos
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true); // Ativar o botão
 
         // Captura/Conecta com a Base de Dados/Firebase
         DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -80,10 +92,44 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R. menu.main, menu);
+        return true;
+    }
+
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_detalhe:
+                abrirLancamentos();
+                return true;
+            case R.id.action_outro_mes:
+                trocarMes();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     public String obterNomeMes(int mes){
         String[] meses = {"Janeiro",
         "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
         return meses[mes];
+    }
+
+    public int obterNumeroMes(String mes){
+        int n = -1;
+        String[] meses = {"Janeiro",
+                "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        for (int i = 0; i < 12; i++){
+            if(meses[i].equals(mes)){
+                n = i+1;
+                break;
+            }
+        }
+        return n;
     }
 
     public void btnCadLancamento(View view){
@@ -92,6 +138,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnDetalheMes(View view){
+       abrirLancamentos();
+    }
+
+    private void trocarMes(){
+        AlertDialog alerta;
+
+        //Define o layout do AlertDialog
+        LayoutInflater inflater = MainActivity.this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_dialog_layout, null);
+        //Cria o gerador do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        //definir layout personalizado
+        builder.setView(dialogView);
+        //define o titulo
+        builder.setTitle("Selecionar outro Mês");
+
+        final TextView txtAno = dialogView.findViewById(R.id.txtAno);
+        final Spinner spMes = dialogView.findViewById(R.id.spMes);
+
+        //define um botão como positivo
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                int mes = obterNumeroMes(spMes.getSelectedItem().toString());
+                String mesEanoSelecionado;
+                if(mes < 10){
+                    mesEanoSelecionado =  "0" + mes + "-" +txtAno.getText().toString();
+                }else{
+                    mesEanoSelecionado =  mes + "-" +txtAno.getText().toString();
+                }
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                // Metodos para passar paramentros entre telas/activitys.
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("mesEano", mesEanoSelecionado);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+        //define um botão como negativo.
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+            }
+        });
+        //cria o AlertDialog
+        alerta = builder.create();
+        //Exibe
+        alerta.show();
+    }
+
+    private void abrirLancamentos(){
         Intent intent = new Intent(this, DetalhesMesActivity.class);
         // Metodos para passar paramentros entre telas/activitys.
         Bundle bundle = new Bundle();
